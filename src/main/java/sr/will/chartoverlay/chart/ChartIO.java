bceecf35679bed4fbb9bfacb84d704182fbe3f3c
Will <willsr71@gmail.com>
2020-06-29 17:51:23 -0700

    Begin rework of chart processing

--- src/main/java/sr/will/chartoverlay/chart/ChartIO.java
+++ src/main/java/sr/will/chartoverlay/chart/ChartIO.java
@@ -1,89 +1,30 @@
 package sr.will.chartoverlay.chart;
 
-import sr.will.chartoverlay.ChartOverlay;
-import sr.will.chartoverlay.chart.kap.KapIO;
 import sr.will.chartoverlay.chart.kap.RasterChart;
-import sr.will.chartoverlay.util.FileUtil;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChartIO {
     public static final String CHART_DIR = "catalog/charts/";
 
-    private static final Map<String, List<Thread>> activeDownloads = new HashMap<>();
-    private static final Map<String, RasterChart> activeCaches = new HashMap<>();
-
-    public static boolean isCurrent(String chart) {
-        File infoFile = new File(CHART_DIR + chart, chart + ".json");
-        if (!Files.exists(infoFile.toPath())) {
-            return false;
-        }
-
-        RasterChart chartInfo = FileUtil.readJson(infoFile, RasterChart.class);
-
-        LocalDate catalogDate = LocalDate.parse(ChartOverlay.catalog.chartsByNumber.get(chart).revisionDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        LocalDate chartDate = LocalDate.parse(chartInfo.header.noticeToMariners.date, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
-
-        return !catalogDate.isAfter(chartDate);
-    }
+    private static final Map<String, ChartProcessThread> processThreads = new HashMap<>();
 
     public static RasterChart getChart(String chart) {
-        if (!ChartOverlay.catalog.chartsByNumber.containsKey(chart)) return null;
+        if (!processThreads.containsKey(chart)) {
+            ChartProcessThread thread = new ChartProcessThread(chart, Thread.currentThread());
+            processThreads.put(chart, thread);
+            thread.start();
+        }
 
-        while (activeDownloads.containsKey(chart)) {
-            if (!activeDownloads.get(chart).contains(Thread.currentThread())) {
-                activeDownloads.get(chart).add(Thread.currentThread());
-            }
-
+        while (!processThreads.get(chart).done) {
             try {
-                ChartOverlay.LOGGER.info("Active download for chart {}, waiting", chart);
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                ChartOverlay.LOGGER.info("Download wait thread interrupted");
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {
+                // Ignored
             }
         }
 
-        if (!bsbExists(chart)) {
-            activeDownloads.put(chart, new ArrayList<>());
-            ChartBSBIO.downloadBSB(chart);
-            File tempFile = ChartPDFIO.downloadPDF(chart);
-            // Interrupt all waiting download threads
-            activeDownloads.remove(chart).forEach(Thread::interrupt);
-        }
-
-
-
-        try {
-            return ChartIO.read(chart);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private static boolean bsbExists(String chart) {
-        String[] list = new File(CHART_DIR).list();
-        if (list == null) return false;
-        return Arrays.asList(list).contains(chart);
-    }
-
-    private static boolean imageExists(String chart) {
-        return Files.exists(new File(CHART_DIR + chart, chart + ".png").toPath());
-    }
-
-    public static void update(String chart) throws IOException {
-        if (isCurrent(chart)) return;
-        KapIO.processRaster(chart);
-    }
-
-    public static RasterChart read(String chart) throws IOException {
-        update(chart);
-        return FileUtil.readJson(new File(CHART_DIR + chart, chart + ".json"), RasterChart.class);
+        return processThreads.get(chart).rasterChart;
     }
 }
---
+++ src/main/java/sr/will/chartoverlay/chart/ChartProcessThread.java
@@ -0,0 +1,83 @@
+package sr.will.chartoverlay.chart;
+
+import sr.will.chartoverlay.ChartOverlay;
+import sr.will.chartoverlay.chart.kap.KapIO;
+import sr.will.chartoverlay.chart.kap.RasterChart;
+import sr.will.chartoverlay.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static sr.will.chartoverlay.chart.ChartIO.CHART_DIR;
+
+public class ChartProcessThread extends Thread {
+    private String chart;
+    private List<Thread> waitingThreads = new ArrayList<>();
+
+    public boolean done = false;
+    public RasterChart rasterChart = null;
+
+    public ChartProcessThread(String chart, Thread initiatingThread) {
+        ChartOverlay.LOGGER.info("New thread {} processing chart {} from thread {}", Thread.currentThread(), chart, initiatingThread);
+
+        this.chart = chart;
+        waitingThreads.add(initiatingThread);
+    }
+
+    public void run() {
+        rasterChart = getRasterChart();
+        done = true;
+
+        waitingThreads.forEach(Thread::interrupt);
+    }
+
+    private RasterChart getRasterChart() {
+        if (!ChartOverlay.catalog.chartsByNumber.containsKey(chart)) return null;
+
+        if (bsbExists(chart)) return read(chart);
+
+        ChartBSBIO.downloadBSB(chart);
+        File tempFile = ChartPDFIO.downloadPDF(chart);
+
+        RasterChart rasterChart = read(chart);
+        ChartPDFIO.writePNG(chart, tempFile, rasterChart);
+        return rasterChart;
+    }
+
+    public static boolean isCurrent(String chart) {
+        File infoFile = new File(CHART_DIR + chart, chart + ".json");
+        if (!Files.exists(infoFile.toPath())) {
+            return false;
+        }
+
+        RasterChart chartInfo = FileUtil.readJson(infoFile, RasterChart.class);
+
+        LocalDate catalogDate = LocalDate.parse(ChartOverlay.catalog.chartsByNumber.get(chart).revisionDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate chartDate = LocalDate.parse(chartInfo.header.noticeToMariners.date, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+
+        return !catalogDate.isAfter(chartDate);
+    }
+
+    public static boolean bsbExists(String chart) {
+        String[] list = new File(CHART_DIR).list();
+        if (list == null) return false;
+        return Arrays.asList(list).contains(chart);
+    }
+
+    public static RasterChart read(String chart) {
+        try {
+            if (!isCurrent(chart)) KapIO.processRaster(chart);
+            return FileUtil.readJson(new File(CHART_DIR + chart, chart + ".json"), RasterChart.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
